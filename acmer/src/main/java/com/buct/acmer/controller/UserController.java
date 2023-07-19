b904602753e8df728aad0024bdb6fb048884f14f
jm <jianmeng_2020@126.com>
2023-07-19 20:37:46 +0800

    后端新增获取当前用户信息方法

--- acmer/src/main/java/com/buct/acmer/controller/UserController.java
+++ acmer/src/main/java/com/buct/acmer/controller/UserController.java
@@ -4,37 +4,39 @@ import com.buct.acmer.entity.PublicProperty;
 import com.buct.acmer.entity.User;
 import com.buct.acmer.service.impl.UserServiceImpl;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.Resource;
 
 @Api(tags = "用户信息")
 @RestController
 @RequestMapping("/acmer/user")
 public class UserController {
-
-    @Resource
+    private User current;//当前登录的用户信息
     private UserServiceImpl userService;
-
-
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
     @ApiOperation("登录用户")
     @PostMapping("/login")
     public PublicProperty login(@RequestBody User user){
-        System.out.println("testuser1");
         User exist = userService.getById(user.getUserId());
-        System.out.println("testuser");
+        current = user;
         if(exist == null){//用户不存在
-            return new PublicProperty(400,"id not exist",null);
+            return new PublicProperty(400,"id wrong",null);
         }else {//判断密码是否相等
             if( exist.getUserPassword().equals(user.getUserPassword()) ){
-                return new PublicProperty(200,"success",null);
+                return new PublicProperty(200,"success",user);
             }else{
                 return new PublicProperty(400,"password wrong",null);
             }
-
         }
+    }
+    @ApiOperation("返回当前登录的用户的信息")
+    @PostMapping("/data")
+    public PublicProperty data(){
+        System.out.println(current.getUserId());
+        return new PublicProperty(400,"success",current);
+
     }
     @ApiOperation("注册用户")
     @PostMapping("/insert")
@@ -52,14 +54,13 @@ public class UserController {
             return new PublicProperty(400,"user exist",null);
         }
     }
-
     @ApiOperation("修改密码")
     @PostMapping("/update")
     public PublicProperty<User> update(@RequestBody User user){
 
         User exist = userService.getById(user.getUserId());
         if(exist!=null){
-            boolean update = userService.updateById(exist);
+            boolean update = userService.updateById(user);
             if(update){
                 return new PublicProperty(200,"success",user);
             }else{
